0932d2abfd779ef0da0b1590e5a8637ba2d7f15d
magbuhosjames <63860349+magbuhosjames@users.noreply.github.com>
2020-12-16 22:53:14 +0800

    re-added say module

---
+++ say.py
@@ -0,0 +1,13 @@
+import win32com.client as wincl
+import pythoncom
+import time
+from pywintypes import com_error
+
+def speak(texttosay):
+    pythoncom.CoInitialize()
+    speak = wincl.Dispatch("SAPI.SpVoice")
+    try:
+        speak.Speak(texttosay)
+    except com_error:
+        time.sleep(1)
+        speak.Speak(texttosay)
